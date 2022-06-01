1f0f42f0e660f1eea045f410a3e65bfef3a47d39
Luisarmandosl <luis-armandosl@hotmail.com>
2022-06-01 11:40:35 -0500

    npm package update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -69,7 +69,7 @@ module.exports.signin = (req,res)=>{
                             
                         } catch(error){
                             console.error(error);
-                            res.status(404).json({message:"User not found"})
+                            return res.status(404).json({message:"User not found"})
                         }
                     })
             })            
