b7fdad25d12627e16c3fd0f3715a9ef673cae19a
Luisarmandosl <luis-armandosl@hotmail.com>
2022-06-09 22:48:39 -0500

    Update record.controller.js

--- controllers/record.controller.js
+++ controllers/record.controller.js
@@ -3,14 +3,6 @@ const credentials = require('../config/credentials.json');
 const jwt = require('jsonwebtoken')
 const config = require('../config/auth.config');
 
-const authUrl = 'https://iam.cloud.ibm.com/identity/token'; 
-const authData = `grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${ credentials.ApiKey }`;
-const authConf = {
-    Headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-};
-
 module.exports.getRecords = (req,res) => {
     const token = req.body.bearerToken;
     const queryURL="https://bpe61bfd0365e9u4psdglite.db2.cloud.ibm.com/dbapi/v4/sql_jobs";
