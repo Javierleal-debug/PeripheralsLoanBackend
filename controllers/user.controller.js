742880f87c86fcb0faf7e17720e28933ecef49d8
Luisarmandosl <luis-armandosl@hotmail.com>
2022-06-10 01:13:33 -0500

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -217,9 +217,9 @@ module.exports.deleteUser = (req,res) => {
         axios.get(getDataUrl,queryConf)
             .then(response => {
                 try{
-                    if(response.data.results[0].error){
+                    if(response.data.results[0].error || response.data.results[0].warning ){
                         console.log(response.data.results[0])
-                        return res.json({message:response.data.results[0].error})
+                        return res.status(404).json({message:"User not found"})
                     }else{
                         console.log(response.data.results[0])
                         res.json({message:"success"})//respuesta con success(json)
